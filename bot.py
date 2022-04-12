341b8b9afbdea7d3200ebff2791c2aa4b4839339
Nathanksa <102903372+Nathanksa@users.noreply.github.com>
2022-04-12 19:49:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-#@mrlokaman 
-#@lntechnical
+#@THANK YOU 
+#@THANKSS FLAC
 from pyrogram import Client, filters
 import requests 
 import json 
@@ -8,28 +8,28 @@ import os
 TOKEN = os.environ.get("TOKEN", "")
 API_ID = int(os.environ.get("API_ID",12345))
 API_HASH = os.environ.get("API_HASH","")
-BITLY_TOKEN = os.environ.get("BITLY_TOKEN","")
+ONCEHELP_TOKEN = os.environ.get("ONCEHELP_TOKEN","")
 
 headers = {
-    'Authorization': BITLY_TOKEN,
+    'Authorization': ONCEHELP_TOKEN,
     'Content-Type': 'application/json',
 }
 
 
-app = Client("bitlybot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
+app = Client("oncehelpbot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client,message):
-  await message.reply_text(f"Hello {message .from_user.first_name}\nhello i am bit.ly short link genrator\n made with love by @mrlokaman ", reply_to_message_id = message.message_id)
+  await message.reply_text(f"Hello {message .from_user.first_name}\nhello i am oncehelp short link genrator\n made  ", reply_to_message_id = message.message_id)
   
 @app.on_message(filters.private & filters.regex("http|https"))
 async def Bitly(client,message):
   URL = message.text
-  DOMAIN = "bit.ly"
+  DOMAIN = "oncehelp.com"
   value  = {'long_url': URL , 'domain': DOMAIN}
   data = json.dumps(value)
   try:
-    r = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers,data = data )
+    r = requests.post('https://api-ssl.oncehelp.com/v4/shorten', headers=headers,data = data )
     result = r.json()
     link = result["link"]
     await message.reply_text(f"```{link}```", reply_to_message_id= message.message_id)
